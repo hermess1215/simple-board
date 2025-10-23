54879c4af966f72d546e7ab055271be8c1dae073
hermess1215 <165272998+hermess1215@users.noreply.github.com>
2025-10-23 14:46:58 +0900

    DB 연동

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 // db.js
 const mysql = require('mysql2/promise');
+require('dotenv').config(); // .env 파일 불러오기
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',          // MySQL 사용자 이름
-  password: 'Hh980131!',   // MySQL 비밀번호
-  database: 'simple_board', // 위에서 만든 데이터베이스 이름
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   connectionLimit: 10
 });
 
